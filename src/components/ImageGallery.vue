d32a8afe53a58dd15ff909971d4472ca743b6e88
mustafa <musmoh73@gmail.com>
2023-10-23 14:17:36 +0300

    image + responsive grid+ loading + lazy image

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,8 +1,28 @@
 <template>
-    <VRow>
-        <VCol v-for="n in 12" :key="n" cols="1">
+    <VRow class="ma-0">
+        <VCol v-for="n in 200" :key="n"
+         cols="6"
+         sm="4"
+         md="3"
+         lg="1"
+         >
             <VCard>
-                hello
+                <VImg 
+                :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                 aspect-ratio="1" cover>
+                    <template v-slot:placeholder>
+                        <VRow 
+                        class="fill-height ma-0" 
+                        align="center" 
+                        justify="center">
+                        <VProgressCircular
+                            indeterminate
+                            color="purple"
+                            >
+                            </VProgressCircular>
+                        </VRow>
+                    </template>
+                </VImg>
             </VCard>
         </VCol>
     </VRow>
