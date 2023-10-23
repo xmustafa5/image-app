fadad31b0a165d65fc82e2aab51bdb0c0b3861d9
mustafa <musmoh73@gmail.com>
2023-10-23 13:09:50 +0300

    slidebar done

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,7 +3,20 @@
           expand-on-hover
             rail
           >
-            hellow
+          <VList>
+          <VListItem
+            prepend-avatar="https://avatars.githubusercontent.com/u/107437940?v=4"
+            title="Mustafa mohammed"
+            subtitle="musmoh73@gmail.com"
+          ></VListItem>
+        </VList>
+        <VDivider></VDivider>
+        <VList density="compact" nav>
+          <VListItem prepend-icon="mdi-image" title="Color Gallery" value="color"></VListItem>
+       </VList>
+       <VList density="compact" nav>
+          <VListItem prepend-icon="mdi-dialpad" title="Grayspace Gallery" value="color"></VListItem>
+       </VList>
           </VNavigationDrawer>
 
 </template>
